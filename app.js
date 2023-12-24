e55d0b0dfab3e2c00d9e98100bcb1e9ac1e7b961
BasmaElhoseny <basmaelhoseny6@gmail.com>
2023-12-24 13:55:00 +0200

    Fixed CROS

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const https = require("https");
+const cors = require('cors');
 const morgan = require("morgan");
+
 
 const userRouter = require(`${__dirname}/routes/userRouter`);
 const matchRouter = require(`${__dirname}/routes/matchRouter`);
@@ -8,6 +10,20 @@ const authRouter = require(`${__dirname}/routes/authRouter`);
 const stadiumRouter = require(`${__dirname}/routes/stadiumRouter`);
 
 const app = express();
+
+// Enable CORS for all routes
+app.use(
+  cors({
+    credentials: true,
+    origin: [
+      // process.env.FRONTDOMAIN,
+      // process.env.CROSSDOMAIN,
+      "http://localhost:3000",
+    ],
+    allowedHeaders: "Content-Type,*",
+    methods: "GET,PUT,POST,DELETE,OPTIONS,PATCH",
+  })
+);
 
 // Development logging
 if (process.env.NODE_ENV === "development") {
